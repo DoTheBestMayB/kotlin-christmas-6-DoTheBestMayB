44f478a37e7cfcded9030070d339126d92340bd0
DoDoBest <send1684@gmail.com>
2023-11-13 02:10:05 +0900

    feat: Validator 클래스를 만든다 & 입력한 방문 날짜가 유효한지 검증하는 함수를 만든다.

---
+++ src/main/kotlin/christmas/domain/Validator.kt
@@ -0,0 +1,32 @@
+package christmas.domain
+
+class Validator {
+
+    private val validBookingDay = 1..31
+
+    fun checkDate(day: Int) = day in validBookingDay
+
+    companion object {
+        private var instance: Validator? = null
+
+        fun getInstance(): Validator {
+            val currentInstance = instance
+            if (currentInstance != null) {
+                return currentInstance
+            }
+            return synchronized(this) {
+                val synchronizedInstance = instance
+                if (synchronizedInstance != null) {
+                    return@synchronized synchronizedInstance
+                }
+                val createdValidator = Validator()
+                instance = createdValidator
+                createdValidator
+            }
+        }
+
+        fun releaseInstance() {
+            instance = null
+        }
+    }
+}

89192a84a225216c2287d43688f6ee4dd14c238b
DoDoBest <send1684@gmail.com>
2023-11-15 04:03:20 +0900

    feat: 할인 정보를 나타내는 Benefit 데이터 클래스 생성

---
+++ src/main/kotlin/christmas/data/Benefit.kt
@@ -0,0 +1,14 @@
+package christmas.data
+
+import java.text.DecimalFormat
+
+data class Benefit(
+    val name: String,
+    val discountAmount: Int,
+) {
+    override fun toString() = "$name: -${priceFormat.format(discountAmount)}원"
+
+    companion object {
+        private val priceFormat = DecimalFormat("#,###")
+    }
+}

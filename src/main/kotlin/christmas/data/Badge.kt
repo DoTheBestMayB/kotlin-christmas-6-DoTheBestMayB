e363323f8dbf48bee0422134844477a113cda836
DoDoBest <send1684@gmail.com>
2023-11-15 19:41:14 +0900

    fix: 명확한 타입 명시로 Badge 테스트 코드 에러 해결

--- src/main/kotlin/christmas/data/Badge.kt
+++ src/main/kotlin/christmas/data/Badge.kt
@@ -5,7 +5,8 @@ enum class Badge(val nickName: String, val minBenefitAmount: Int) {
     TREE("트리", 10_000), SANTA("산타", 20_000);
 
     companion object {
-        fun from(benefitAmount: Int) = entries.sortedBy {
+        // 리턴 타입으로 Badge를 명시하지 않으면 BadgeTest.kt 에서 Badge에 대해 Unresolved Reference Error 발생
+        fun from(benefitAmount: Int): Badge = entries.sortedBy {
             -it.minBenefitAmount
         }.firstOrNull {
             it.minBenefitAmount <= benefitAmount
--- src/test/kotlin/christmas/data/BadgeTest.kt
+++ src/test/kotlin/christmas/data/BadgeTest.kt
@@ -1,6 +1,6 @@
 package christmas.data
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -9,49 +9,49 @@ class BadgeTest {
     
     @ParameterizedTest
     @ValueSource(ints = [20_000, 30_000, 999_999])
-    @DisplayName("BADGE - SANTA")
+    @DisplayName("Badge - SANTA")
     fun `2만 원 이상의 혜택을 받으면 SANTA를 반환한다`(benefitAmount: Int) {
         // when
         val actual = Badge.from(benefitAmount)
 
         // then
         val expected = Badge.SANTA
-        assertThat(actual).isEqualTo(expected)
+        Assertions.assertThat(actual).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [19_999, 15_000, 10_000])
-    @DisplayName("BADGE - TREE")
+    @DisplayName("Badge - TREE")
     fun `1만 원 이상 2만 원 미만의 혜택을 받으면 TREE를 반환한다`(benefitAmount: Int) {
         // when
         val actual = Badge.from(benefitAmount)
 
         // then
         val expected = Badge.TREE
-        assertThat(actual).isEqualTo(expected)
+        Assertions.assertThat(actual).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [5_000, 5_001, 7_689, 9_999])
-    @DisplayName("BADGE - STAR")
+    @DisplayName("Badge - STAR")
     fun `5천 원 이상 1만 원 미만의 혜택을 받으면 STAR를 반환한다`(benefitAmount: Int) {
         // when
         val actual = Badge.from(benefitAmount)
 
         // then
         val expected = Badge.STAR
-        assertThat(actual).isEqualTo(expected)
+        Assertions.assertThat(actual).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [-1_000, -1, 0, 1, 2, 100, 1_000, 4_999])
-    @DisplayName("BADGE - NONE")
+    @DisplayName("Badge - NONE")
     fun `그 외의 혜택을 받으면 NONE을 반환한다`(benefitAmount: Int) {
         // when
         val actual = Badge.from(benefitAmount)
 
         // then
         val expected = Badge.NONE
-        assertThat(actual).isEqualTo(expected)
+        Assertions.assertThat(actual).isEqualTo(expected)
     }
 }
